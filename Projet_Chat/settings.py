d727bff38b4d0d3166185d4362bc61de5900aae2
Greehon <gabriel.malegue2@gmail.com>
2026-01-19 15:31:38 +0100

    ajout css

--- Projet_Chat/settings.py
+++ Projet_Chat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5a_rkr!vua#u2v^529$fd*^=-g$lxm@q9zq(58@u(dmh+!mmvl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["guillaume4ssig.alwaysdata.net"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -118,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'  
 
