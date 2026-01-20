f7cecc574387026938f539f64e443b92ac55a53a
Guillaume S <gu.dev@laposte.net>
2026-01-20 16:13:30 +0100

    ajout de quoi voir les salons et messages dans le panneau d'admin

--- chat_messages/admin.py
+++ chat_messages/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Salon, Message
 
-# Register your models here.
+@admin.register(Salon)
+class SalonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'created_at')
+    search_fields = ('name',)
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'salon', 'sender', 'date')
+    list_filter = ('salon', 'sender')
+    search_fields = ('content',)
